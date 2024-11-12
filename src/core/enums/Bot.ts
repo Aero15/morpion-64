4634a07dbf5472797210743d5f5ea8f994a10e06
Aero15 <Aero15@users.noreply.github.com>
2024-11-12 23:06:04 +0100

    Création d'une énumération de pour définir le temps de réponse des bots

---
+++ src/core/enums/Bot.ts
@@ -0,0 +1,13 @@
+import Point from "$core/entity/Point.svelte"
+
+// Delai de réponse des bots
+export enum BotDelay {
+    Slow = 0,
+    Default = 1,
+    Fast = 2,
+}
+
+// Configuration du temps de réponse des bots
+export let slowBotSpeedDelay: Point = new Point(1800, 4000)
+export let defaultBotSpeedDelay: Point = new Point(1500, 3000)
+export let fastBotSpeedDelay: Point = new Point(500, 1500)

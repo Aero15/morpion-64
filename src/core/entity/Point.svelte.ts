8c62f8ec50021a8a0602a6ff5e6d3ed27b8f6250
Aero15 <Aero15@users.noreply.github.com>
2024-11-12 20:41:05 +0100

    Création d'une classe Point

---
+++ src/core/entity/Point.svelte.ts
@@ -0,0 +1,32 @@
+export default class Point {
+    private _x: number = $state(0);
+    private _y: number = $state(0);
+
+    constructor(
+        x: number,
+        y: number
+    ) {
+        this._x = x;
+        this._y = y;
+    }
+  
+    // Getter pour x
+    get x(): number {
+        return this._x;
+    }
+  
+    // Setter pour x
+    set x(value: number) {
+        this._x = value;
+    }
+  
+    // Getter pour y
+    get y(): number {
+        return this._y;
+    }
+  
+    // Setter pour y
+    set y(value: number) {
+        this._y = value;
+    }
+}

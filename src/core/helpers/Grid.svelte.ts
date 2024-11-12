2b9116c62d600007ee1183e3ff95176984029620
Aero15 <Aero15@users.noreply.github.com>
2024-11-12 22:36:27 +0100

    Création de fonctions pour réaliser des calculs sur une grille de jeu

---
+++ src/core/helpers/Grid.svelte.ts
@@ -0,0 +1,15 @@
+import type Point from "$core/entity/Point.svelte";
+
+// Calcul du nombre de cases à partir de la taille de la grille
+export function countCellsFor(
+    gridSize: Point
+): number {
+    return gridSize.x * gridSize.y
+}
+
+// Calcul du nombre max de participants (pouvant jouer 3 symboles) à partir du nombre de cases
+export function calcMaxNbParticipantsFrom(
+    numberOfCells: number
+): number {
+    return Math.floor(numberOfCells / 3)
+}

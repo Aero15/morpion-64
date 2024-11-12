d630829fea194c9330b16fc8e128d5ad21044cc6
Aero15 <Aero15@users.noreply.github.com>
2024-11-12 20:42:17 +0100

    [Slider] Bugfix: Barre de progression à taille non responsive

--- src/lib/form/Slider.svelte
+++ src/lib/form/Slider.svelte
@@ -89,7 +89,7 @@
         margin: 5px;
         padding: 0;
         min-width: 1.3rem;
-        width: calc((18rem + 2px) * (var(--percent) / 100) - (var(--margin) * 2));
+        width: calc((100%) * (var(--percent) / 100) - (var(--margin) * 2));
         height: calc((3rem + 2px) - (var(--margin) * 2));
         border-radius: calc(1rem - (var(--margin) + 1px));
         box-shadow: 0 0 0 2px var(--tint) inset;

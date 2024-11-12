d1e14ad5a427acc5f522a2615925d1bb838166d2
Aero15 <Aero15@users.noreply.github.com>
2024-11-12 23:27:04 +0100

    Création des énumérations pour les couleurs, symboles et type de joueurs

---
+++ src/core/enums/Color.ts
@@ -0,0 +1,20 @@
+export enum Color {
+    Marine = '#0000ff',
+    Blue = '#0087ff',
+    Cyan = '#00ffff',
+    Teal = '#008080',
+    
+    Purple = '#800080',
+    Lavender = '#886dA9',
+    Yellow = '#ffff00',
+    Lime = '#00ff00',
+
+    Red = '#ff0000',
+    Pink = '#ff00aa',
+    Orange = '#ffaa00',
+    Green = '#008800',
+
+    DarkRed = '#a52a2a',
+    Fuchsia = '#CD0E67', 
+    Coral = '#ff7f50',
+}

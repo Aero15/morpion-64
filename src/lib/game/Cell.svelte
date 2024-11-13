576f26e4ccf9db97a9603ff697e2ffa7471f3c6a
Aero15 <Aero15@users.noreply.github.com>
2024-11-13 21:15:51 +0100

    [Grid/Cell] Création des composants du plateau de jeu

---
+++ src/lib/game/Cell.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import Point from "$core/entity/board/Point.svelte";
+    import Icon from "$lib/shared/Icon.svelte";
+    import { scale } from "svelte/transition";
+
+    interface Props {
+        eraserEnabled?: boolean
+        position: Point,
+        symbol?: string,
+        color?: string,
+        lock: boolean
+        onClick?: (position: Point) => void
+    }
+
+    let {
+        eraserEnabled = $bindable(false),
+        position = $bindable(new Point(0, 0)),
+        symbol = $bindable(undefined),
+        color = $bindable(undefined),
+        lock = $bindable(false),
+        onClick = () => {},
+    }: Props = $props();
+
+    let isEmpty = $derived(symbol === undefined);
+    let disabled = $derived( // Désactiver la cellule lorsque :
+        (eraserEnabled && isEmpty) || // le mode effaceur est activé et la cellule est vide
+        (!eraserEnabled && !isEmpty) || // le mode effaceur n'est pas activé et la cellule n'est pas vide
+        lock // la cellule est bloquée
+    );
+</script>
+
+<button
+    class="cell"
+    onclick={() => onClick(position)}
+    {disabled}
+>
+    {#if symbol}
+        <div transition:scale style:color={color}>
+            <Icon icon={symbol} size={64} />
+        </div>
+    {/if}
+</button>
+
+<style>
+    .cell {
+        aspect-ratio: 1;
+        padding: 0;
+        margin: 0;
+        width: 80px;
+        border: 1px solid rgba(127,127,127,1);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 10px;
+        transition: background .2s, border-color .2s, color .2s;
+
+        &:disabled {
+            border-color: transparent;
+            background: transparent;
+            color: inherit;
+        }
+
+        &:not(:disabled) {
+            cursor: pointer;
+            background: transparent;
+
+            &:hover {
+                background: var(--main_color);
+            }
+        }
+    }
+</style>
---
+++ src/lib/game/Grid.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+    import type Cell from "$core/entity/board/Cell.svelte";
+    import Point from "$core/entity/board/Point.svelte";
+    import CellView from "./Cell.svelte";
+
+    interface Props {
+        grid: Cell[][],
+        lock?: boolean,
+        eraserEnabled?: boolean,
+        displayHalo?: boolean
+        onCellClick?: (position: Point) => void
+    }
+
+    let {
+        grid = $bindable<Cell[][]>(),
+        lock = $bindable(false),
+        displayHalo = $bindable(false),
+        eraserEnabled = $bindable(false),
+        onCellClick = () => {},
+    }: Props = $props();
+</script>
+
+<div
+    class="grid"
+    class:lock
+    class:halo={displayHalo}
+>
+    {#each grid as row}
+        <div class="row" class:lock style="grid-template-columns: repeat({ row.length }, 1fr);">
+            {#each row as { position, symbol, color }}
+                <CellView
+                    { position } { symbol }
+                    { color } { lock }
+                    bind:eraserEnabled
+                    onClick={ onCellClick }
+                />
+            {/each}
+        </div>
+    {/each}
+</div>
+
+<style>
+    @property --angle{
+        syntax: "<angle>";
+        initial-value: 0deg;
+        inherits: false;
+    }
+
+    @keyframes spin{
+        from{
+            --angle: 0deg;
+        }
+        to{
+            --angle: 360deg;
+        }
+    }
+
+    div {
+        display: grid;
+        transition: all .3s;
+        gap: 0;
+
+        &:not(.lock) {
+            gap: 6px;
+        }
+    }
+
+    .grid {
+        --border-width: .5rem;
+        --radius : 2rem;
+
+        padding: 2rem;
+        position: relative;
+        background: #fff;
+        border-radius: var(--radius);
+        box-shadow: 0 5px 30px rgba(0,0,0,.3);
+
+        &::after, &::before {
+            content: '';
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            background-image: conic-gradient(
+                from var(--angle),
+                #ff4545, #00ff99,#006aff,
+                #ff0095, #ff4545
+            );
+            top: 50%;
+            left: 50%;
+            translate: -50% -50%;
+            z-index: -1;
+            opacity: 0;
+            padding: 0;
+            border-radius: calc(var(--radius) + var(--border-width));
+            animation: 3s spin linear infinite;
+            transition: opacity .2s, padding .2s;
+        }
+
+        &.halo {
+            &::before, &::after {
+                padding: var(--border-width);
+            }
+            &::before {
+                opacity: 1;
+            }
+            &::after {
+                opacity: .9;
+                filter: blur(2.5rem);
+            }
+        }
+
+        .row {
+            position: relative;
+            z-index: 1;
+        }
+    }
+
+    canvas {
+        position: absolute;
+        inset: 0 0 0 0;
+        z-index: 1;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .grid {
+            background: #323232;
+            box-shadow: 0 0 0 1px rgba(255,255,255,.3), 0 5px 30px rgba(0,0,0,.3);
+        }
+    }
+</style>

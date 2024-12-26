54318278ce875da84d97d14bbaecb035c70c954c
Aero15 <Aero15@users.noreply.github.com>
2024-12-26 13:28:19 +0100

    [Game] Leaderboard : improve contrast and colors

--- src/lib/game/Leaderboard.svelte
+++ src/lib/game/Leaderboard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import GameEngine from "$core/entity/engine/GameEngine.svelte";
+    import { improveContrast } from "$core/helpers/Colors.svelte";
     import Icon from "$lib/shared/Icon.svelte";
 
     interface Props {
@@ -11,6 +12,11 @@
     }: Props = $props();
 
     let ranking = $derived(game.players.players.toSorted((a, b) => b.score - a.score))
+
+    function improveColor(color: string) {
+        let { light, dark } = improveContrast(color);
+        return `light-dark(${light}, ${dark})`;
+    }
 </script>
 
 <table class="bx-leaderboard">
@@ -23,18 +29,22 @@
     </thead>
 
     <tbody>
-        {#each ranking as player, index}
+        {#each ranking as { color, name, symbol, score }, index}
             <tr>
                 <td><p class="rank">{index + 1}</p></td>
                 <td>
                     <div class="identity">
-                        <p style:color={player.color} class="symbol">
-                            <Icon icon={player.symbol} size={18} />
+                        <p style:color={improveColor(color)} class="symbol">
+                            <Icon icon={symbol} size={18} />
                         </p>
-                        <p class="name">{player.name}</p>
+                        <p class="name">{name}</p>
                     </div>
                 </td>
-                <td><p class="score">{player.score}</p></td>
+                <td>
+                    <p class="score">
+                        <strong>{score}</strong>
+                    </p>
+                </td>
             </tr>
         {/each}
     </tbody>
@@ -66,6 +76,16 @@
             padding: .25rem 0;
 
             .symbol { font-size: 0; }
+        }
+
+        .score strong {
+            background: linear-gradient(
+                to right,
+                light-dark(rgb(255, 0, 200), rgb(255, 145, 243)),
+                light-dark(rgb(0, 153, 255), cyan)
+            );
+            background-clip: text;
+            color: transparent;
         }
     }
 </style>

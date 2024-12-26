155862ef3858a044aca9f3cea878044865a816e3
Aero15 <Aero15@users.noreply.github.com>
2024-12-26 18:27:14 +0100

    New roller component

---
+++ src/lib/shared/Roller.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { createRawSnippet, type Snippet } from "svelte";
+
+    interface Props {
+        delay?: number,
+        children: Snippet
+    }
+
+    let {
+        delay = 0,
+        children
+    }: Props = $props();
+</script>
+
+<main class="bx-roller" style:--delay={delay * 70 + 'ms'}>
+    {@render children()}
+</main>
+
+<style>
+    .bx-roller {
+        --delay: 0s;
+        animation: reveal .5s forwards;
+        animation-iteration-count: 1;
+        animation-delay: var(--delay);
+        padding-top: 100px;
+        opacity: 0;
+    }
+
+    @keyframes reveal {
+        0% {
+            padding-top: 100px;
+            opacity: 0;
+        }
+        100% {
+            padding-top: 0;
+            opacity: 1;
+        }
+    }
+</style>

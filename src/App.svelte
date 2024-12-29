f39b883e9f5f225bbbedb924ef22a826e227afb4
Aero15 <aero15@outlook.com>
2024-12-29 18:54:44 +0100

    [App] Refactoring responsive design

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,34 @@
 <script lang="ts">
     import { routes } from "$core/routes";
-    import Router from "svelte-spa-router";
     import Header from "$lib/global/Header.svelte";
     import NavBar from "$lib/global/NavBar.svelte";
+    import Router, { location } from "svelte-spa-router";
     import Responsive from "$lib/shared/Responsive.svelte";
     import type { BreakpointSize } from "$core/enums/BreakpointSize";
 
     let size: BreakpointSize = $state('sm');
 </script>
 
-<main>
+<main class="bx-app"
+    class:small-width={['sm', 'md'].includes(size) && $location != '/game'}>
     <Responsive bind:size />
 
-    <Header />
+    {#if $location != '/game'}
+        <Header />
+    {/if}
 
     <Router { routes } />
 
-    {#if ['sm', 'md', 'lg'].includes(size)}
+    {#if ['sm', 'md', 'lg'].includes(size) && $location != '/game'}
         <NavBar />
     {/if}
 </main>
+
+<style>
+    .bx-app {
+        &.small-width {
+            padding-top: 51px;
+            padding-bottom: 64px;
+        }
+    }
+</style>

2c597703c468b1cba487eb08d555ace946a7fbf0
Aero15 <Aero15@users.noreply.github.com>
2025-01-01 17:22:26 +0100

    [Settings] Split page into tabs

--- src/page/Settings.svelte
+++ src/page/Settings.svelte
@@ -16,50 +16,104 @@
     import type Point from "$core/entity/board/Point.svelte";
     import GridSizeStats from "$lib/settings/GridSizeStats.svelte";
     import GridPresetSelector from "$lib/settings/GridPresetSelector.svelte";
+    import type { BreakpointSize } from "$core/enums/BreakpointSize";
+    import Responsive from "$lib/shared/Responsive.svelte";
+    import TabBar from "$lib/shared/TabBar.svelte";
+
+    let size: BreakpointSize = $state('sm');
+    let Tabs = {
+        GridSize: 0,
+        ResponseTime: 1,
+    }
+    let tabs = [
+        { name: 'Plateau de jeu', icon: 'squares', id: Tabs.GridSize },
+        { name: 'Temps de réponse', icon: 'bot', id: Tabs.ResponseTime },
+    ]
+    let selectedId = $state(Tabs.GridSize);
 
     function speedToText(speed: Point): string {
         return `Entre ${speed.x / 1000} et ${speed.y / 1000} secondes`;
     }
 </script>
 
-<Jumbo icon="config" title="Réglages" />
+<Responsive bind:size />
+
+<Jumbo icon="config" title="Réglages">
+    <span></span>
+</Jumbo>
+
+<div class="tabs">
+    <TabBar tabs={tabs} bind:selectedId variant="rounded" />
+</div>
 
 <PageWrap>
-    <div class="grid page">
-        <Section title="Dimension du plateau">
-            <div style="display: grid; gap: 1rem">
-                <GridSizeStats />
-                <GridPresetSelector large />
+    <div id="pg-settings">
+        {#if selectedId == Tabs.GridSize}
+            <Section title="Dimension du plateau">
+                <div class="grid grid-size">
+                    <GridSizeStats large={['xl', '2xl'].includes(size)} />
+                    <GridPresetSelector large />
+                </div>
+            </Section>
+        {/if}
+
+        {#if selectedId == Tabs.ResponseTime}
+            <div class="response-time">
+                <div>
+                    <Section title="Comportement des bots">
+                        <p style:margin="0 0 .5rem"><strong>Delai de réponse</strong></p>
+                        
+                        <RadioGroup>
+                            <Radio bind:group={$botSpeedDelay}
+                                label="Lent" value={BotDelay.Slow}
+                                description={ speedToText(slowBotSpeedDelay) } />
+        
+                            <Radio bind:group={$botSpeedDelay}
+                                label="Normal" value={BotDelay.Default}
+                                description={ speedToText(defaultBotSpeedDelay) } />
+        
+                            <Radio bind:group={$botSpeedDelay}
+                                label="Rapide" value={BotDelay.Fast}
+                                description={ speedToText(fastBotSpeedDelay) } />
+                        </RadioGroup>
+                    </Section>
+                </div>
             </div>
-        </Section>
-
-        <Section title="Comportement des bots" delay={1}>
-            <p style:margin="0 0 .5rem"><strong>Delai de réponse</strong></p>
-            
-            <RadioGroup>
-                <Radio bind:group={$botSpeedDelay}
-                    label="Lent" value={BotDelay.Slow}
-                    description={ speedToText(slowBotSpeedDelay) } />
-
-                <Radio bind:group={$botSpeedDelay}
-                    label="Normal" value={BotDelay.Default}
-                    description={ speedToText(defaultBotSpeedDelay) } />
-
-                <Radio bind:group={$botSpeedDelay}
-                    label="Rapide" value={BotDelay.Fast}
-                    description={ speedToText(fastBotSpeedDelay) } />
-            </RadioGroup>
-        </Section>
+        {/if}
     </div>
 </PageWrap>
 
 <style>
+    #pg-settings {
+        display: flex;
+        flex-direction: column;
+    }
     .grid { display: grid; }
-    .page { gap: clamp(.5rem, 8vw, 4rem); }
+
+    .grid-size {
+        gap: 1rem;
+    }
+
+    .tabs {
+        margin: -2rem 0;
+    }
+
+    .response-time {
+        display: flex;
+        justify-content: center;
+        div { max-width: 600px; flex: 1; }
+    }
 
     @media (width >= 400px) {
-        .page {
+        .grid-size {
             grid-template-columns: repeat( auto-fit, minmax(400px, 1fr) );
+            align-items: center;
+        }
+    }
+
+    @media (width >= 850px) {
+        .grid-size :global(.bx-grid_stats) {
+            order: 2;
         }
     }
 </style>

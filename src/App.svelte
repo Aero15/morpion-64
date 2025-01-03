a74d494f44dea62aa8ba6b13d7a7818cad36ba36
Aero15 <aero15@outlook.com>
2025-01-03 17:50:54 +0100

    [I18n] Load translation

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
     import { routes } from "$core/routes";
+    import { isLoading } from 'svelte-i18n';
     import Header from "$lib/global/Header.svelte";
     import NavBar from "$lib/global/NavBar.svelte";
     import Router, { location } from "svelte-spa-router";
     import Responsive from "$lib/shared/Responsive.svelte";
     import type { BreakpointSize } from "$core/enums/BreakpointSize";
+    import LoadingI18n from "$lib/global/LoadingI18n.svelte";
+    import I18n from "$lib/global/I18n.svelte";
 
     let size: BreakpointSize = $state('sm');
 
@@ -12,20 +15,26 @@
     let inGame = $derived.by(() => paths.find(path => $location.startsWith(path)) !== undefined);
 </script>
 
-<main class="bx-app"
-    class:small-width={['sm', 'md'].includes(size) && !inGame}>
-    <Responsive bind:size />
+<I18n />
 
-    {#if !inGame}
-        <Header />
-    {/if}
+{#if $isLoading}
+    <LoadingI18n />
+{:else}
+    <main class="bx-app"
+        class:small-width={['sm', 'md'].includes(size) && !inGame}>
+        <Responsive bind:size />
 
-    <Router { routes } />
+        {#if !inGame}
+            <Header />
+        {/if}
 
-    {#if ['sm', 'md', 'lg'].includes(size) && !inGame}
-        <NavBar />
-    {/if}
-</main>
+        <Router { routes } />
+
+        {#if ['sm', 'md', 'lg'].includes(size) && !inGame}
+            <NavBar />
+        {/if}
+    </main>
+{/if}
 
 <style>
     .bx-app {

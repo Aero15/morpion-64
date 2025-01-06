3c09becabd9549d0fbe5a9e78370967d9c9891b6
Aero15 <Aero15@users.noreply.github.com>
2025-01-06 19:34:47 +0100

    [PlayerTurn] Display current player's name

--- src/lib/player/PlayerTurn.svelte
+++ src/lib/player/PlayerTurn.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import GameEngine from "$core/entity/engine/GameEngine.svelte";
     import type Player from "$core/entity/player/Player.svelte";
+    import { improveContrast } from "$core/helpers/Colors.svelte";
     import ListBubbles from "./ListBubbles.svelte";
 
     interface Props {
@@ -16,6 +17,16 @@
     }: Props = $props();
 
     let players: Player[] = $derived(sort(game.players.players, game.players.currentTurn))
+    let current: Player = $derived(players[0])
+
+    let contrastColor = $derived.by(() => {
+        return improveContrast(current.color);
+    })
+
+    let symbolColor = $derived.by(() => {
+        let { light, dark } = contrastColor;
+        return `light-dark(${light}, ${dark})`;
+    })
 
     function sort(
         arr: Player[],
@@ -30,4 +41,8 @@
 
 <div class="bx-player_turn" style:display="grid">
     <ListBubbles players={players} {compact} {limit} />
+
+    <p style:color={symbolColor} style:margin={0}>
+        <strong>{ current.name }</strong>
+    </p>
 </div>
